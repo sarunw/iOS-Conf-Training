f5641d511a5049ef58cf2e8d72bbde2e0be6e871
Sarun Wongpatcharapakorn <artwork.th@gmail.com>
2022-01-17 16:46:04 +0700

    Add mobsfscan

--- iOS-Shack-v2/ViewController.swift
+++ iOS-Shack-v2/ViewController.swift
@@ -32,10 +32,6 @@ class ViewController: UIViewController, MenuControllerDelegate {
             print(error.localizedDescription)
         }
         
-        
-        let aws_access_key = "AKIAIOSFODNN7EXAMPLE"
-        let aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-        
         menu.delegate = self
 
         sideMenu = SideMenuNavigationController(rootViewController: menu)
